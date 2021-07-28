0d654ca5366337615b6999dd5020d842fc92ceda
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-07-27 21:25:26 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,82 @@
+<?php
+
+/* Se oculta los demas archivos para evitar demasiadas salidas a pantalla
+
+require('global.php');
+
+require('list.php');
+
+require('funciones.php');  
+
+require('strict.php');*/
+
+require('anonimas.php');
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="Jesus Miranda">
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="INDEX,FOLLOW">
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <link rel="stylesheet" href="assets/css/all.css">
+    <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="assets/css/responsivo.css">
+    <link rel="stylesheet" href="assets/css/estilos.css">
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <link rel="icon" type="image/png" href="">
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <title></title>
+
+
+</head>
+
+<body>
+
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <header>
+
+
+        <h1><?php echo 'Hola mundo' ?></h1>
+
+
+    </header>
+
+
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <main>
+
+    </main>
+
+
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <footer>
+
+
+    </footer>
+
+
+    <script src="assets/js/app.js"></script>
+
+</body>
+
+</html>
