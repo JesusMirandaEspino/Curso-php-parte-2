2e0783842bfcdf2f8c0152920c3f9f4accf127f9
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-07-29 21:51:27 -0500

    isset

--- index.php
+++ index.php
@@ -1,6 +1,67 @@
 <?php
 
 
+if(isset($_POST['nombre'] ) ){
+
+    $nombre = $_POST['nombre'];
+    $pass = $_POST['pass'];
+    $comentarios = $_POST['comentarios'];
+    $idioma = $_POST['idioma'];
+    $estado = $_POST['estado'];
+    $pasatiempos = $_POST['hobbies'];
+    $comidas = $_POST['comidas'];
+
+    $salto = '</br>';
+
+
+    $error = [];
+
+    if ($nombre === '') {
+        array_push($error, '<p>' .  'Por favor escribe el nombre'  . '</p>');
+    }
+
+    if ($pass === '') {
+        array_push($error, '<p>' .  'Por favor escribe la clave'  . '</p>');
+    }
+
+    if ($comentarios === '') {
+        array_push($error, '<p>' .  'Por favor escribe los comentarios'  . '</p>');
+    }
+
+    if( isset($pasatiempos ) ){
+        if( count($pasatiempos) == 0 ){
+            array_push($error, '<p>' .  'Por escoge algun pasatiempo'  . '</p>');
+        }
+    }
+
+    if (count($error) > 0) {
+
+        foreach ($error as $err) {
+            echo '<p>' .  $err .   '</p>';
+        }
+    } else {
+
+        echo 'Bienvenido ' .  $nombre . $salto;
+        echo 'Password jeje: ' .  $pass . $salto;
+        echo 'Comentarios ' .  $comentarios . $salto;
+        echo 'Idioma ' .  $idioma . $salto;
+        echo 'Estado ' .  $estado . $salto;
+
+
+
+        foreach ($pasatiempos as $pasatiempo) {
+            echo 'Pasatiempo ' .  $pasatiempo . $salto;
+        }
+
+        foreach ($comidas as $comida) {
+            echo 'Comida: ' .  $comida . $salto;
+        }
+    }
+    
+
+
+}
+
 
 
 ?>
@@ -50,7 +111,7 @@
             <div class="contenedor1">
 
 
-                <form action="validar.php" method="POST" class="col-full-12">
+                <form action="index.php" method="POST" class="col-full-12">
 
                     <label for="nombre">Nombre</label>
                     <input type="text" name="nombre" id="nombre">
